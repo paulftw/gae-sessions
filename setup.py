7afa3f3b39a73edafebb8d1f87a8fc154eddb62c
Paul Korzhyk <paul.korzhyk@gmail.com>
2011-07-21 23:46:42 +1000

    That was a wrong setup.py

--- setup.py
+++ setup.py
@@ -1,23 +1,40 @@
-# -*- coding: utf-8 -*-
-"""Setup script."""
+"""
+GAE-Sessions
+-----------
 
-import os
-from setuptools import setup, find_packages
+Fast, lightweight Sessions middleware for Google App Engine
+(memcache + datastore)
+
+Links
+`````
+
+* `documentation <http://wiki.github.com/dound/gae-sessions/>`_
+* `development version
+  <http://github.com/dound/gae-sessions/zipball/master#egg=GAE-Sessions-dev>`_
+"""
+from setuptools import setup
+
 
 setup(
-    name='app',
-    version='0.1',
-    author="Paul Korzhyk",
-    author_email="paul@scifiware.com",
-    description="app engine skeleton",
-    url='',
-    packages=find_packages('app'),
-    package_dir = {'': 'app'},
-    include_package_data=True,
-    install_requires=[
-        'flask',
-        'jinja2',
-        'werkzeug',
-    ],
+    name='GAE-Sessions',
+    version='1.07',
+    url='http://github.com/dound/gae-sessions',
+    license='Apache License 2.0',
+    author='David Underhill',
+    author_email='DAVID, WHAT"S YOUR EMAIL??? ***',
+    description='Sessions middleware for Google App Engine',
+    packages=['gaesessions'],
+    namespace_packages=['gaesessions'],
     zip_safe=False,
+    platforms='any',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
 )
